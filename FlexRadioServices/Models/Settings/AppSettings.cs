92af8e33e260277b23cf00143cb3b986e06f1162
Jeff Uchitjil <git@k9kld.org>
2026-03-09 13:09:50 -0500

    fix(Docker): Update docker dev file documentation

--- FlexRadioServices/Models/Settings/AppSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace FlexRadioServices.Models.Settings;
-
-public class AppSettings
-{
-    public static RadioSettings RadioSettings { get; set; } = new RadioSettings();
-
-    public static MqttBrokerSettings MqttBrokerSettings { get; set; } = new MqttBrokerSettings();
-}
